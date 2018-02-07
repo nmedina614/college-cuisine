3b4f9d53b1431ef4e5e73f54f5e3f0495fb20332
Aaron Melhaff <booglessmith@gmail.com>
2018-02-07 16:20:52 -0500

    Added nav, slides, and cards to homepage

--- index.php
+++ index.php
@@ -9,10 +9,11 @@ require_once('vendor/autoload.php');
 $f3 = Base::instance();
 
 $f3->route('GET /', function($f3) {
+    $f3->set('title', 'College Cuisine');
+    $f3->set('content', 'views/home.html');
 
-
-    $view = new View;
-    echo $view->render('pages/base.html');
+    $template = new Template();
+    echo $template->render('views/base.html');
 });
 
 $f3->run();
