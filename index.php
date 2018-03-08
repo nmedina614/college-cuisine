45667e49f56133c2dcae28a59d831c78f3dd81ff
Aaron Melhaff <booglessmith@gmail.com>
2018-03-08 18:23:43 -0500

    Changed filenames and template structure

--- index.php
+++ index.php
@@ -18,21 +18,59 @@ Model::connect();
 // Homepage route.
 $f3->route('GET /', function($f3) {
 
-    $f3->set('title', 'College Cuisine');
-    $f3->set('content', 'views/home.html');
+    // Title to use in template.
+    $title = "College Cuisine";
 
+    // List of paths to stylesheets.
+    $styles = array(
+        //$f3->get('BASE').'/styles/main.css',
+        //$f3->get('BASE').'/styles/login.css'
+    );
 
+    // List of paths for sub-templates being used.
+    $includes = array(
+        'views/_nav.html',
+        'views/_home.html'
+    );
 
+    // List of paths to scripts being used.
+    $scripts = array();
+
+    $f3->set('title',    $title);
+    $f3->set('styles',   $styles);
+    $f3->set('includes', $includes);
+    $f3->set('scripts',  $scripts);
+
+    // Display Template
     $template = new Template();
-    echo $template->render('views/base.html');
+    echo $template->render('views/_base.html');
 });
 
 // Login route.
 $f3->route('GET|POST /login', function($f3) {
 
-    $f3->set('title', 'Login');
-    $f3->set('content',
-        'views/login.html');
+    // Title to use in template.
+    $title = "Login";
+
+    // List of paths to stylesheets.
+    $styles = array(
+        //$f3->get('BASE').'/styles/main.css',
+        //$f3->get('BASE').'/styles/login.css'
+    );
+
+    // List of paths for sub-templates being used.
+    $includes = array(
+        'views/_nav.html',
+        'views/_login.html'
+    );
+
+    // List of paths to scripts being used.
+    $scripts = array();
+
+    $f3->set('title',    $title);
+    $f3->set('styles',   $styles);
+    $f3->set('includes', $includes);
+    $f3->set('scripts',  $scripts);
 
     if(isset($_POST['submit'])) {
 
@@ -59,18 +97,37 @@ $f3->route('GET|POST /login', function($f3) {
     session_unset();
 
     $template = new Template();
-    echo $template->render('views/base.html');
+    echo $template->render('views/_base.html');
 });
 
 
 // Sumbit Recipe route
 $f3->route('GET|POST /recipe/@recipeID', function($f3, $params) {
 
+    // Title to use in template.
+    $title = $params['recipeID'];
+
+    // List of paths to stylesheets.
+    $styles = array();
+
+    // List of paths for sub-templates being used.
+    $includes = array(
+        'views/_nav',
+        'views/_recipe.html'
+    );
+
+    // List of paths to scripts being used.
+    $scripts = array();
+
+    $f3->set('title',    $title);
+    $f3->set('styles',   $styles);
+    $f3->set('includes', $includes);
+    $f3->set('scripts',  $scripts);
+
     //SQL to get Recipe Name
-    $f3->set('title', $params['recipeID']);
-    $f3->set('content',
-        'views/recipe.html');
+
     $f3->set('recipeID', $params['recipeID']);
+
     //print_r($_POST);
 
     if(isset($_POST['submit'])) {
@@ -80,14 +137,30 @@ $f3->route('GET|POST /recipe/@recipeID', function($f3, $params) {
 
 
     $template = new Template();
-    echo $template->render('views/base.html');
+    echo $template->render('views/_base.html');
 });
 
 $f3->route('GET /profiles/@user', function($f3, $params) {
-    //SQL to get Recipe Name
-    $f3->set('title', $params['user']);
-    $f3->set('content',
-        'views/profile.html');
+
+    // Title to use in template.
+    $title = $params['user'];
+
+    // List of paths to stylesheets.
+    $styles = array();
+
+    // List of paths for sub-templates being used.
+    $includes = array(
+        'views/_nav.html',
+        'views/_profile.html'
+    );
+
+    // List of paths to scripts being used.
+    $scripts = array();
+
+    $f3->set('title',    $title);
+    $f3->set('styles',   $styles);
+    $f3->set('includes', $includes);
+    $f3->set('scripts',  $scripts);
 
     if(isset($_POST['submit'])) {
         Model::login($_POST['username'],$_POST['password']);
