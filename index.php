fd03236ccc5e9e7c27efa6875df403c532d66018
Aaron Melhaff <booglessmith@gmail.com>
2018-03-08 16:08:24 -0500

    Added user profile route and template

--- index.php
+++ index.php
@@ -83,5 +83,21 @@ $f3->route('GET|POST /recipe/@recipeID', function($f3, $params) {
     echo $template->render('views/base.html');
 });
 
+$f3->route('GET /profiles/@user', function($f3, $params) {
+    //SQL to get Recipe Name
+    $f3->set('title', $params['user']);
+    $f3->set('content',
+        'views/profile.html');
+
+    if(isset($_POST['submit'])) {
+        Model::login($_POST['username'],$_POST['password']);
+    }
+
+
+
+    $template = new Template();
+    echo $template->render('views/base.html');
+});
+
 
 $f3->run();
