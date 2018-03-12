7ed6d86c01e01130ebe7dd198b9337a5cd7e0e98
Aaron Melhaff <booglessmith@gmail.com>
2018-03-12 16:07:35 -0400

    Readded new recipe route

--- index.php
+++ index.php
@@ -110,6 +110,37 @@ $f3->route('GET|POST /login', function($f3) {
     echo $template->render('views/_base.html');
 });
 
+// Submit new Recipe
+$f3->route('GET|POST /recipe/new-recipe', function($f3) {
+    // Title to use in template.
+    $title = "Submit your Recipe!";
+    // List of paths to stylesheets.
+    $styles = array(
+        // If you need a stylesheet do
+        //$f3->get('BASE').'/assets/styles/STYLESHEET-NAME.css
+    );
+    // List of paths for sub-templates being used.
+    $includes = array(
+        'views/_nav.html',
+        'views/_submit-recipe.html'
+    );
+    // List of paths to scripts being used.
+    $scripts = array(
+        // If you need a script do
+        $f3->get('BASE').'/assets/scripts/recipe-scripts.js'
+    );
+    $f3->set('title',    $title);
+    $f3->set('styles',   $styles);
+    $f3->set('includes', $includes);
+    $f3->set('scripts',  $scripts);
+    //print_r($_POST);
+    if(isset($_POST['submit'])) {
+        Model::login($_POST['username'],$_POST['password']);
+    }
+    $template = new Template();
+    echo $template->render('views/_base.html');
+});
+
 
 // Submit Recipe route
 $f3->route('GET|POST /recipe/@recipeID', function($f3, $params) {
